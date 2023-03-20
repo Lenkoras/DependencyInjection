d04d2021ac43748ebec91a66242c5d026daef2de
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-03-20 18:38:55 +0300

    Added service type checks to GetRequiredService extension.

--- DependencyInjection/Extensions/ServiceProviderGetRequiredServiceExtensions.cs
+++ DependencyInjection/Extensions/ServiceProviderGetRequiredServiceExtensions.cs
@@ -6,7 +6,23 @@
         /// <exception cref="Exception"></exception>
         public static object GetRequiredService(this IServiceProvider serviceProvider, Type serviceType)
         {
-            return serviceProvider.GetService(serviceType);
+            ArgumentNullException.ThrowIfNull(serviceProvider);
+            ArgumentNullException.ThrowIfNull(serviceType);
+
+            var providerResult = serviceProvider.GetService(serviceType);
+            if (providerResult is not null)
+            {
+                var resultType = providerResult.GetType();
+                if (resultType.IsAssignableTo(serviceType))
+                {
+                    return providerResult;
+                }
+                else
+                {
+                    throw new Exception($"Required service ({serviceType.FullName}) can not be resolved. The service provider returned object with type ({resultType.FullName}) not assignable to {serviceType.FullName}.");
+                }
+            }
+            throw new Exception($"Required service ({serviceType.FullName}) can not be resolved. The service provider returned null.");
         }
 
         /// <exception cref="ArgumentNullException"></exception>
