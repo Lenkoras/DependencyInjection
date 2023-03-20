4b920c470a8022f561665eead7042e730779e668
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-03-20 18:40:41 +0300

    Added comment block to the GetService provider extension & null checks. GetRequiredService moved out in other class.

---
+++ DependencyInjection/Extensions/ServiceProviderGetServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace DependencyInjection.Extensions
+{
+    public static class ServiceProviderGetServiceExtensions
+    {
+        /// <summary>
+        /// Gets the service object of the specified <typeparamref name="TService"/> type.
+        /// </summary>
+        /// <typeparam name="TService">A type of service object to get.</typeparam>
+        /// <param name="serviceProvider">Service provider.</param>
+        /// <returns>A service object of <see langword="typeof"/> <typeparamref name="TService"/>. -or- null if there is no service object of type serviceType.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TService? GetService<TService>(this IServiceProvider serviceProvider) where TService : class
+        {
+            ArgumentNullException.ThrowIfNull(serviceProvider);
+
+            return (TService?)serviceProvider.GetService(typeof(TService));
+        }
+    }
+}
