3c1c2280feb4772e1f195ae48f3dd57562bac610
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-03-20 18:37:24 +0300

    GetRequiredService extensions moved out in other class.

---
+++ DependencyInjection/Extensions/ServiceProviderGetRequiredServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace DependencyInjection.Extensions
+{
+    public static class ServiceProviderGetRequiredServiceExtensions
+    {
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="Exception"></exception>
+        public static object GetRequiredService(this IServiceProvider serviceProvider, Type serviceType)
+        {
+            return serviceProvider.GetService(serviceType);
+        }
+
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="Exception"></exception>
+        public static TService GetRequiredService<TService>(this IServiceProvider serviceProvider) where TService : class =>
+            (TService)GetRequiredService(serviceProvider, typeof(TService));
+    }
+}
